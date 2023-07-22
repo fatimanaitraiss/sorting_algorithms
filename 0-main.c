80ffc3e228c8014c28f819a201bef815598094c6
fatimanaitraiss <fatimanaitraiss33@gmail.com>
2023-07-22 08:38:12 -0700

    commit second

--- 0-main.c
+++ 0-main.c
@@ -19,21 +19,21 @@ int main(void)
 
 {
 
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 
-    size_t n = sizeof(array) / sizeof(array[0]);
+size_t n = sizeof(array) / sizeof(array[0]);
 
 
-    print_array(array, n);
+print_array(array, n);
 
-    printf("\n");
+printf("\n");
 
-    bubble_sort(array, n);
+bubble_sort(array, n);
 
-    printf("\n");
+printf("\n");
 
-    print_array(array, n);
+print_array(array, n);
 
-    return (0);
+return (0);
 
 }
