3572f09cff44991bd6a186f2d98f0f5972e84f8b
Creationdis <Creationdis@gmail.com>
2023-07-24 09:32:23 -0700

    kqjslkqd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
 
@@ -16,56 +14,37 @@ void bubble_sort(int *array, size_t size)
 {
 
 	size_t j;
-
 	int swapped;
-
 	size_t temp;
-
 	size_t tempsize;
 
-
 	if (array == NULL)
-
 		return;
 
 	tempsize = size;
-
 	swapped = 1;
 
 	while (swapped)
-
 	{
-
 		swapped = 0;
-
 		j = 0;
 
 		while (j < tempsize - 1)
-
 		{
-
 			if (array[j] > array[j + 1])
-
 			{
-
 				temp = array[j + 1];
-
 				array[j + 1] = array[j];
-
 				array[j] = temp;
-
-				print_array(array, size);
-
-				swapped = 1;
+				print_arra(array, size);
+				swapped = 1
 
 			}
 
 			j++;
-
 		}
 
 		tempsize = j;
-
 	}
 
 }
